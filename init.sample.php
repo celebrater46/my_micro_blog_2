b8e139eef1f17dedea763962d8d8c202b93d19de
celebrater <tkgm45nbchk@gmail.com>
2022-04-05 06:43:51 +0900

    moved classes into classes directory

--- init.sample.php
+++ init.sample.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace my_micro_blog;
+
+ini_set('display_errors', 1);
+
+const MMB_PATH = "";
+const MMB_FCM_PATH = "";
+const MMB_HCM_PATH = "";
+const MMB_MAX_ARTICLES_PER_PAGE = 2;
+const MMB_FOLD = false;
+const MMB_COMMENT = false;
+const MMB_COMMENT_PERMIT = false;
+const MMB_NEW_COMMENTS_ = 5;
+const MMB_NEW_ARTICLES_ = 5;
